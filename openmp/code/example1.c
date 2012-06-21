88517503a64b0cb6f966a9a164c1eea05bd04734
Raghesh <raghesh.a@gmail.com>
2012-06-21 13:23:56 +0530

    Some style changes

--- openmp/code/example1.c
+++ openmp/code/example1.c
@@ -1,12 +1,13 @@
+// To compile: gcc example1.c -fopenmp
 #include <omp.h>
 #include <stdio.h>
 
-int main()  {                                 
-  int iam =0, np = 1;     
+int main() {        
+  int iam =0, np = 1;
 #pragma omp parallel private(iam, np)
   {
-#if defined (_OPENMP) 
-    np = omp_get_num_threads(); 
+#if defined (_OPENMP)
+    np = omp_get_num_threads();
     iam = omp_get_thread_num();
 #endif
     printf("Hello from thread %d out of %d \n", iam, np);
