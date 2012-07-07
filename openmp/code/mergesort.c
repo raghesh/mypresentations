e75a6f21d8f67d0861ef35986b2c42c141a3e72b
raghesh <raghesh@revathy.(none)>
2012-07-07 11:44:52 +0530

    Merge serial corrected

--- openmp/code/mergesort.c
+++ openmp/code/mergesort.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DATASIZE 10
+
 void merge(int A[], int p, int q, int r) {
   int i, j, k;
   int n1 = q - p;
   int n2 = r - q;
 
-  int *L = malloc((n1 + 1)*sizeof(int));
-  int *R = malloc((n2 + 1)*sizeof(int));
+  int *L = (int *)malloc((n1 + 1)*sizeof(int));
+  int *R = (int *)malloc((n2 + 1)*sizeof(int));
 
   for (i = 0; i < n1; i++)
     L[i] = A[p + i];
@@ -15,8 +17,8 @@ void merge(int A[], int p, int q, int r) {
   for (i = 0; i < n2; i++)
     R[i] = A[q + i];
 
-  L[n1] = 0xfffff;
-  R[n2] = 0xfffff;
+  L[n1] = 0xfffffff;
+  R[n2] = 0xfffffff;
   i = 0; j = 0;
   for (k = p; k < r; k++) {
     if (L[i] <= R[j]) {
@@ -33,6 +35,10 @@ void merge(int A[], int p, int q, int r) {
 
 
 void mergeSortSerial(int A[], int p, int r) {
+  printf("%d %d: ", p, r);
+  for (int i = 0; i < DATASIZE; i++)
+    printf("%d ", A[i]);
+  printf("\n");
   if (p == r - 1)
     return;
   int q = (p + r) / 2;
@@ -46,6 +52,7 @@ void mergeSortOMPParallel(int A[], int p, int r, int threads) {
     mergeSortSerial(A, p, r);
   else if (threads > 1) {
     int q = (p + r) / 2;
+    int iam = 0, np = 1;
     #pragma omp parallel sections
     {
       #pragma omp section
@@ -53,14 +60,38 @@ void mergeSortOMPParallel(int A[], int p, int r, int threads) {
       #pragma omp section
       mergeSortOMPParallel(A, q, r, threads - threads / 2);
     }
+#if defined (_OPENMP)
+    np = omp_get_num_threads();
+    iam = omp_get_thread_num();
+#endif
+    printf("Hello from thread %d out of %d \n", iam, np);
     merge(A, p, q, r);
   }
 }
-int main() {
-  int A[10] = {4, 2, 3, 1, 6, 5, 8, 10, 7, 9}, i;
 
-  mergeSortOMPParallel(A, 0, 10, 2);
+int main(int argc, char *argv[]) {
+  unsigned int seed;
+  //int A[DATASIZE] = {100, 2, 78, 34, 1, 5, 0, 234, 9, 0}, r, i;
+  //int A[DATASIZE] = {100, 2, 78, 34}, r, i;
+  int A[DATASIZE] = {4404747, 1048575, 1048575, 1048575, 0, 1048575, 0, 135049, 0, 0},  r, i;
 
-  for (i = 0; i < 10; i++)
-    printf("%d\n", A[i]);
+  if (argc != 2) {
+	  fprintf(stderr, "Usage: %s <seed>\n", argv[0]);
+	  exit(EXIT_FAILURE);
+  }
+/*  seed = atoi(argv[1]);
+ srand(seed);
+  for (i = 0; i < DATASIZE; i++) {
+    r =  rand();
+    A[i] = r;
+  }*/
+  for (i = 0; i < DATASIZE; i++)
+    printf("%d  ", A[i]);
+  printf("\n");
+  mergeSortSerial(A, 0, DATASIZE);
+  //mergeSortOMPParallel(A, 0, DATASIZE, 1);
+
+  for (i = 0; i < DATASIZE; i++)
+    printf("%d  ", A[i]);
+  printf("\n");
 }
