5f3395e4d63bddf862a0a11f3e1b996681674bff
raghesh <raghesh@revathy.(none)>
2012-07-24 12:13:49 +0530

    Removing openmp.pdf

---
+++ openmp/code/example5_parallel_for.c
@@ -0,0 +1,27 @@
+// To compile: gcc example1.c -fopenmp
+#include <omp.h>
+#include <stdio.h>
+
+#define SIZE 20
+int main() {        
+  int a[SIZE], b[SIZE], c[SIZE];
+  int i, tid;
+
+  for(i = 0; i < SIZE; i++) {
+    a[i] = i;
+    b[i] = 2 * i;
+  }
+
+#pragma omp parallel  shared(a), private(i, tid)
+  {
+#pragma omp for
+    for(i = 0; i < SIZE; i++) {
+      c[i] = a[i] + b[i];
+      tid = omp_get_thread_num();
+      printf("Thread %d, i = %d\n", tid, i);
+    }
+  }
+
+  for(i = 0; i < SIZE; i++)
+    printf("%d\n", c[i]);
+}
