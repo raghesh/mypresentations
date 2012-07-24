9cfe77eef72b23eff500daf0422382e05c70ef65
raghesh <raghesh@revathy.(none)>
2012-07-24 12:35:06 +0530

    Adding reduction clause

---
+++ openmp/code/example6_reduction.c
@@ -0,0 +1,24 @@
+// To compile: gcc example1.c -fopenmp
+#include <omp.h>
+#include <stdio.h>
+
+#define SIZE 10
+int main() {        
+  int a[SIZE], b[SIZE], c[SIZE];
+  int i, tid, result = 0;
+
+  for(i = 0; i < SIZE; i++) {
+    a[i] = i;
+    b[i] = 2 * i;
+  }
+
+#pragma omp parallel shared(a), private(i, tid), reduction(+:result)
+  {
+#pragma omp for
+    for(i = 0; i < SIZE; i++) {
+      result += a[i] + b[i];
+    }
+  }
+
+  printf("result = %d\n", result);
+}
