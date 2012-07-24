a49a30312e89bb988fe92ae484850626888963d2
raghesh <raghesh@revathy.(none)>
2012-07-24 11:16:33 +0530

    Adding firstprivate clause

---
+++ openmp/code/example4_firstprivate.c
@@ -0,0 +1,14 @@
+// To compile: gcc example1.c -fopenmp
+#include <omp.h>
+#include <stdio.h>
+
+int main() {        
+  int b = 20, tid = 0;
+#pragma omp parallel private(tid) firstprivate(b)
+  {
+    b++;
+    tid = omp_get_thread_num();
+    printf("Thread %d: b = %d\n", tid, b);
+  }
+  printf("b = %d\n", b);
+}
